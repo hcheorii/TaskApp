91cbfb9655ba4569ed8e417ee086065e27648374
hyeon cheol lee <zkxmzhrhtn@naver.com>
2024-05-19 23:01:13 +0900

    LogItem 컴포넌트 완성

--- src/components/LoggerModal/LogItem/LogItem.tsx
+++ src/components/LoggerModal/LogItem/LogItem.tsx
@@ -1,7 +1,28 @@
-import React from "react";
-
-const LogItem = () => {
-    return <div>LogItem</div>;
+import React, { FC } from "react";
+import { ILogItem } from "../../../types";
+import { BsFillPersonFill } from "react-icons/bs";
+import { logItemWrap, author, message, date } from "./LogItem.css";
+type TLogItemProps = {
+    logItem: ILogItem;
+};
+const LogItem: FC<TLogItemProps> = ({ logItem }) => {
+    let timeOffset = new Date(Date.now() - Number(logItem.logTimestamp));
+    const showOffsetTime = `
+    ${timeOffset.getMinutes() > 0 ? `${timeOffset.getMinutes()}m` : ""}
+    ${timeOffset.getSeconds() > 0 ? `${timeOffset.getSeconds()}s ago` : ""}
+    ${timeOffset.getSeconds() === 0 ? `just now` : ""}
+    `;
+    return (
+        <div className={logItemWrap}>
+            <div className={author}>
+                <BsFillPersonFill />
+                {logItem.logAuthor}
+                {/* 사람모양 아이콘 */}
+            </div>
+            <div className={message}>{logItem.logMessage}</div>
+            <div className={date}>{showOffsetTime}</div>
+        </div>
+    );
 };
 
 export default LogItem;
